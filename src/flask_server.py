4ef5d8eeacf4bd1ab9fd08462c64534a063fd8cd
CosmicOriginDev <154019149+CosmicOriginDev@users.noreply.github.com>
2025-10-28 10:19:27 -0500

    got data to go to results page

--- src/flask_server.py
+++ src/flask_server.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, send_from_directory, request, make_response
+from flask import Flask, render_template, send_from_directory, request, make_response, redirect, url_for
 from flask_socketio import SocketIO
 import os
 import uuid
 import book_finder
 import google_books_api
+import book
 
 base_dir = os.path.abspath(os.path.join(os.getcwd(), "..")) 
 template_dir = os.path.join(base_dir, "templates")
@@ -11,6 +12,7 @@ static_dir = os.path.join(base_dir, "static")
 app = Flask(__name__, template_folder=template_dir,static_folder=static_dir)
 socketio=SocketIO(app)
 print()
+best_book = book.Book("", "", 0, 0, book.Condition.UNKNOWN, book.Medium.UNKNOWN)
 
 @app.route("/")
 def home(): #renamed this from "results" to "home" for clarity
@@ -25,10 +27,17 @@ def getStatic():
 
 @app.route("/results")
 def results_page():
-    return render_template("ResultsPage.html")
+    global best_book
+    title = best_book.title
+    isbn = best_book.isbn
+    price = best_book.price
+    link = best_book.link
+    print(title)
+    return render_template("ResultsPage.html", link = link, title = title, price = price, isbn = isbn)
 
 @socketio.on("Go_button_pushed")
 def go(data):
+    global best_book
     session_id = request.cookies.get("session_id")
     user_search = data.get("search", "")
     googleBooksAPIObject = google_books_api.GoogleBooksAPI()
@@ -36,7 +45,8 @@ def go(data):
     print(f"[Session {session_id}] User searched for: {user_search}")
     result = book_finder.find_cheapest_book(isbn)
     if result != None:
-        print(result.link)
+        best_book = result
+        socketio.emit('redirect',url_for('results_page'))
     else:
         print("No books found :(")
 
