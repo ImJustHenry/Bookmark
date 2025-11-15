b7a8bf4bc1c35a9a160eeee710a57d0f4a6c839b
Mehul Antony <mehulantony10@gmail.com>
2025-11-14 20:26:15 -0600

    added additional unit tests

--- tests/server_route_test.py
+++ tests/server_route_test.py
@@ -5,9 +5,13 @@ sys.path.insert(0, os.path.abspath("./src"))
 from flask_server import app
 
 class TestServer(unittest.TestCase):
-    #flask gives a test client for us to simulate the server
+    #Set up Flask test client for each test.
     def setUp(self):
         self.client = app.test_client()
+
+    def test_app_starts(self):
+        response = self.client.get("/")
+        self.assertIn(response.status_code, [200,500])
 
     #server should not crash when loading homepage.
     def test_load_homepage(self):
@@ -15,10 +19,20 @@ class TestServer(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Bookmark!", response.data)
 
+    # Ensures the UI search field exists.
+    def test_homepage_has_search_input(self):
+        response = self.client.get("/")
+        self.assertIn(b'id="search_input"', response.data)
+
     #If status code 200, page loads. If 400,no book selected logic works.
     def test_results_not_crashing(self):
         response = self.client.get("/results")
         self.assertTrue(response.status_code in [200,400])
+
+    # Ensures that the search query parameter exists
+    def test_results_with_query_parameter(self):
+        response = self.client.get("/results?query=test")
+        self.assertIn(response.status_code, [200,400])
         
 if __name__ == "__main__":
     unittest.main()
