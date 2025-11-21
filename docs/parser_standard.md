ab6e3825bb2957a6c552d7afd2147f6573fa9319
CosmicOriginDev <154019149+CosmicOriginDev@users.noreply.github.com>
2025-11-21 16:02:16 -0600

    added abebook parser now errors properly

--- docs/parser_standard.md
+++ docs/parser_standard.md
@@ -1,9 +1,14 @@
 # Parser standard
 
-A parser must have this function with the following signature:
+A parser must have the functions with the following signatures:
 
 ```{python}
     parse(int isbn) -> Book
+    get_test_isbn() -> int
 ```
+
+parse function should raise a BookError exception if the book is not found.
+
+get_test_isbn function should output an isbn that exists on the target site.
 
 Note that not all parameters of the Book object are required to be filled for the input.
--- src/book_finder.py
+++ src/book_finder.py
@@ -1,22 +1,34 @@
-from parsers import *
-import book
+import sys
+PARSER_PATH = "./parsers/"
+parser_modules = []
+sys.path.insert(0,PARSER_PATH)
+import importlib
+import os
+
+
+def import_parsers():
+    filenames = os.listdir(PARSER_PATH)
+    for f in filenames:
+        if f.endswith(".py"):
+            name = f[:-3]
+            module = importlib.import_module(name)
+            parser_modules.append(module)
+
+def _run_parser(parser_class,isbn, book_list):
+    out = None
+    try:
+        out = parser_class.parse(isbn)
+    except:
+        return
+    else:
+        book_list.append(out)
 
 def find_cheapest_book(isbn):
-
+    import_parsers()
     book_objects = []
-
-    abebooks_book = abebook_parser.parse(isbn)
-    macmillan_book = macmillan_parser.parse(isbn)
-    if abebooks_book!=None:
-        book_objects.append(abebooks_book)
-    
-    
-    """
-    chegg_book = chegg_scraper.get_chegg_prices(isbn)
-    if chegg_book!=None:
-        book_objects.append(chegg_book)
-    """
-
+    for p in parser_modules:
+        _run_parser(p,isbn,book_objects)
+    print(book_objects)
     if book_objects == []:
         return None
     
