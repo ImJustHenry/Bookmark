2e166bb7552dd3c7f3ce7fb3c7a83857badf5b16
CosmicOriginDev <154019149+CosmicOriginDev@users.noreply.github.com>
2025-12-06 12:08:53 -0600

    Rename TestDocker class to DockerTest

--- tests/docker_test.py
+++ tests/docker_test.py
@@ -1,7 +1,7 @@
 import unittest
 import subprocess
 
-class TestDocker(unittest.TestCase):
+class DockerTest(unittest.TestCase):
     def docker_test(self):
         result = subprocess.run(["docker", "build", "--check", "."])
         self.assertEqual(result.returncode,0)
