47416c09a5b2add971b088d62b1478a23a3f3059
Revateesa Dammalapati <revateesadammalapati@Revateesas-MacBook-Pro.local>
2025-12-06 11:10:08 -0700

    Revert "Merge pull request #49 from ImJustHenry/docker-unit-test"
    This reverts commit 1637cedb3562092ef6512a310a50b6832c832653, reversing
    changes made to 53a31c0e4395ba43b27d9731cc1e88d884f9232a.

--- tests/docker_test.py
+++
@@ -1,7 +0,0 @@
-import unittest
-import subprocess
-
-class TestDocker(unittest.TestCase):
-    def docker_test(self):
-        result = subprocess.run(["docker", "build", "--check", "."])
-        self.assertEqual(result.returncode,0)
